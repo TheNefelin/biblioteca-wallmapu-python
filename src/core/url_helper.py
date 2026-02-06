5d07ad0c31fc59a56a206c89848fec74a28b728d
Francisco Carmona <flcarmonac@yahoo.com>
2026-02-06 18:39:58 -0300

    Update

--- src/core/url_helper.py
+++ src/core/url_helper.py
@@ -4,4 +4,4 @@ def get_base_url(request: Request) -> str:
   return f"{request.url.scheme}://{request.url.netloc}{request.url.path}"
 
 def get_static_news_url(request: Request) -> str:
-  return f"{request.url.scheme}://{request.url.netloc}/static/news"
+  return f"{request.url.scheme}://{request.url.netloc}/static/news/"
