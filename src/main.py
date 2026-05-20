3ac10f89f80018446c200cd076483b8d1fc08f38
Francisco Carmona <flcarmonac@yahoo.com>
2026-05-20 18:48:07 -0400

    Update for Prod

--- src/main.py
+++ src/main.py
@@ -37,7 +37,7 @@ app = FastAPI(title="Biblioteca Wallmapu API", description="In development", ver
 app.add_middleware(
   CORSMiddleware,
   allow_origins=[
-    "http://localhost:4200",
+    #"http://localhost:4200",
     "https://biblioteca-wallmapu-angular.vercel.app",
     "https://wallmapumesana.cl"
   ],
